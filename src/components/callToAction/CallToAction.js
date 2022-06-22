27842cc07d7ad5d89459309220ae1acff21123d1
Esther Olusa <oyinolusa1@gmail.com>
2022-06-22 23:27:23 +0100

    cta, blog anf footer implementation

--- src/components/callToAction/CallToAction.js
+++ src/components/callToAction/CallToAction.js
@@ -1,8 +1,18 @@
 import React from "react";
-import classes from "./calltoaction.module.css";
+import "./calltoaction.css";
 
 const CallToAction = () => {
-  return <div>CallToAction</div>;
+  return (
+    <div className="gpt3__cta">
+      <div className="gpt3__cta-content">
+        <p>Request Early Access to Get Started</p>
+        <h3>Register today & start exploring the endless possiblities.</h3>
+      </div>
+      <div className="gpt3__cta-btn">
+        <button type="button">Get Started</button>
+      </div>
+    </div>
+  );
 };
 
 export default CallToAction;
